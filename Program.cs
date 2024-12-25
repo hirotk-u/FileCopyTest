bcc32d6b91f4206b0dfe4be3603031efed19d552
hirotk U <hiro.sacredstar2@gmail.com>
2024-12-26 00:11:58 +0900

    ・日付指定できるよう修正 ・読み込み先をコンフィグ設定可能に修正

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FileCopyTest
 {
     internal class Program
     {
+        private static readonly string SRC_DIR = "srcDir";
+        private static readonly string DEST_DIR = "destDir";
+
         /// <summary>
         /// Main
         /// </summary>
@@ -16,23 +18,35 @@ namespace FileCopyTest
         {
             try
             {
-                // 今日の日付を取得
-                string today = DateTime.Now.ToString("yyyyMMdd");
+                Console.WriteLine("指定した日付以降のファイルをコピーします");
+                Console.WriteLine("日付を指定してください(yyyyMMdd)");
+                string inputDay = Console.ReadLine();
 
                 // コピー元とコピー先のディレクトリを指定
-                string sourceDirectory = @"..\..\TEST\SrcFolder";
-                string destinationDirectory = @"..\..\TEST\DestFolder";
+                string srcDir = ConfigurationManager.AppSettings[SRC_DIR];
+                string destDir = ConfigurationManager.AppSettings[DEST_DIR];
+
+                //入力日付変換
+                CultureInfo jaJP = new CultureInfo("ja-JP");
+                DateTime targetDate;
+                bool parseResult = DateTime.TryParseExact(inputDay, "yyyyMMdd", jaJP, DateTimeStyles.AssumeLocal, out targetDate);
+
+                if (!parseResult)
+                {
+                    throw new Exception("日付フォーマットが正しくありません");
+                }
 
                 // コピー元ディレクトリ内のファイルとサブフォルダを再帰的に取得
-                CopyFilesRecursively(sourceDirectory, destinationDirectory, today);
+                CopyFilesRecursively(srcDir, destDir, targetDate);
 
                 Console.WriteLine("ファイルコピー完了！");
-                Console.ReadLine();
             }
             catch (Exception e)
             {
                 Console.WriteLine($"エラー発生[{e.Message}]");
             }
+
+            Console.ReadKey();
         }
 
         /// <summary>
@@ -40,8 +54,8 @@ namespace FileCopyTest
         /// </summary>
         /// <param name="sourcePath"></param>
         /// <param name="destinationPath"></param>
-        /// <param name="today"></param>
-        static void CopyFilesRecursively(string sourcePath, string destinationPath, string today)
+        /// <param name="targetDate"></param>
+        static void CopyFilesRecursively(string sourcePath, string destinationPath, DateTime targetDate)
         {
             // コピー先ディレクトリが存在しない場合は作成
             if (!Directory.Exists(destinationPath))
@@ -55,11 +69,10 @@ namespace FileCopyTest
                 // ファイル名を取得
                 string fileName = Path.GetFileName(file);
 
-                // ファイルの作成日を取得
-                DateTime creationDate = File.GetLastWriteTime(file);
+                // ファイルの更新日を取得
+                DateTime updDate = File.GetLastWriteTime(file);
 
-                // ファイルの作成日が今日の日付と一致する場合、コピーする
-                if (creationDate.ToString("yyyyMMdd") == today)
+                if (targetDate <= updDate)
                 {
                     string destFile = Path.Combine(destinationPath, fileName);
                     File.Copy(file, destFile, true);
@@ -71,7 +84,7 @@ namespace FileCopyTest
             foreach (string directory in Directory.GetDirectories(sourcePath))
             {
                 string destDirectory = Path.Combine(destinationPath, Path.GetFileName(directory));
-                CopyFilesRecursively(directory, destDirectory, today);
+                CopyFilesRecursively(directory, destDirectory, targetDate);
             }
         }
     }
